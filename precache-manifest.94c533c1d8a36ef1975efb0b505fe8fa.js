d039b91658aa283698332660c1b5092e3ee64b04
aotearoan <andrew@aotearoan.com>
2021-01-03 12:19:48 +0000

    gh-pages

---
+++ precache-manifest.94c533c1d8a36ef1975efb0b505fe8fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df04b45044b2087ad5c",
+    "url": "/sass-farbig/css/app.6a49deff.css"
+  },
+  {
+    "revision": "0e906acad13479b00e6384a094ff2475",
+    "url": "/sass-farbig/index.html"
+  },
+  {
+    "revision": "8df04b45044b2087ad5c",
+    "url": "/sass-farbig/js/app.aca26567.js"
+  },
+  {
+    "revision": "c3e863555720e62a92dd",
+    "url": "/sass-farbig/js/chunk-vendors.2aabd25f.js"
+  },
+  {
+    "revision": "91a5b1449e973a0036c6c51055292050",
+    "url": "/sass-farbig/manifest.json"
+  }
+]);
